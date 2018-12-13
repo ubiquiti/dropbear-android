9c3536f06a52f0a29eed131081eaaf379deddeea
Geofferey <geofferey.eakins@gmail.com>
2018-12-12 22:20:14 -0800

    remove dropbear src & update build.sh

--- build.sh
+++ build.sh
@@ -19,15 +19,26 @@ fi
 
 # Setup the environment
 export TARGET=../target
+
 # Specify binaries to build. Options: dropbear dropbearkey scp dbclient
-export PROGRAMS="dropbear dropbearkey dbclient dropbearconvert scp"
+if [ -z ${PROGRAMS} ]; then
+    export PROGRAMS="dropbear dropbearkey dbclient dropbearconvert scp"
+fi
+
 # Which version of Dropbear to download for patching
 export VERSION=2018.76
 
+#Download the dropbear source if not found
+if [ ! -f ./dropbear-$VERSION.tar.bz2 ]; then
+    wget -O ./dropbear-$VERSION.tar.bz2 https://matt.ucc.asn.au/dropbear/releases/dropbear-$VERSION.tar.bz2
+fi
+
+# Set default toolchain if none specified
 if [ -z ${TOOLCHAIN} ]; then
     export TOOLCHAIN=$CWD/android-r11c-standalone-toolchain
 fi
-# Download the latest version of dropbear SSH
+
+# Download the r11c standalone arm toolchain
 if [ ${TOOLCHAIN} = $CWD/android-r11c-standalone-toolchain ] && [ ! -d $CWD/android-r11c-standalone-toolchain ]; then
     echo ""
     echo "Fetching r11c standalone toolchain..."
@@ -37,8 +48,8 @@ fi
 echo ""
 
 # Start each build with a fresh source copy
-#rm -rf ./dropbear-$VERSION
-#tar xjf dropbear-$VERSION.tar.bz2
+rm -rf ./dropbear-$VERSION
+tar xjf dropbear-$VERSION.tar.bz2
 
 # Change to dropbear directory
 cd dropbear-$VERSION
@@ -57,13 +68,12 @@ export CC="$COMPILER --sysroot=$SYSROOT"
 # Android 5.0 Lollipop and greater require PIE. Default to this unless otherwise specified.
 if [ -z $DISABLE_PIE ]; then export CFLAGS="-g -O2 -pie -fPIE"; else echo "Disabling PIE compilation..."; fi
 sleep 5
+
 # Use the default platform target for pie binaries
 unset GOOGLE_PLATFORM
 
 # Apply the new config.guess and config.sub now so they're not patched
-#cp ../config.guess ../config.sub .
-
-make clean
+cp ../config.guess ../config.sub .
 
 ./configure --host=$HOST --disable-zlib --disable-largefile --disable-shadow --disable-utmp --disable-utmpx --disable-wtmp --disable-wtmpx --disable-pututxline --disable-lastlog > /dev/null 2>&1
 
@@ -75,17 +85,15 @@ echo
 ### END -- configure without modifications first to generate files
 
 # Begin applying changes to make Android compatible
+
 # Apply the compatibility patch
-#patch -p1 < ../android-compat.patch
-#patch -p1 < ../dropbear-getpass-2018.patch
-#patch -p1 < ../openpty.patch
+patch -p1 < ../dropbear-2018.76-android.patch
+
 cd -
 
 echo "Compiling for ARM"
 
 cd dropbear-$VERSION
-
-make clean
 
 ./configure --host=$HOST --disable-zlib --disable-largefile --disable-shadow --disable-utmp --disable-utmpx --disable-wtmp --disable-wtmpx --disable-pututxline --disable-lastlog
 
@@ -121,4 +129,3 @@ if [ $MAKE_SUCCESS -eq 0 ]; then
 else
  	echo "Compilation failed."
 fi
-
--- dropbear-2018.76/netbsd_getpass.h
+++
@@ -1,4 +0,0 @@
-#ifndef __GETPASS_H__
-#define __GETPASS_H__
-extern char *getpass (const char *prompt);
-#endif
